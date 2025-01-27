ab6761d69d1719be58c025b82688df05d91ff0f6
yan <ladeiatortelli8@outlook.com>
2025-01-27 18:50:52 -0300

    Atualizar a lógica de produtos e adicionar estilo para 'Em breve'

--- src/components/Produtos.js
+++ src/components/Produtos.js
@@ -1,31 +1,38 @@
 import React from 'react';
 import '../styles/Produtos.css'; // Importando o CSS para a estilização dos cards
 
+// Importando a mesma imagem para todos os produtos
+import palhaninho from '../images/palhaninho.jpeg';
+import palhatradiconal from '../images/palhatradicional.jpg';
+import inter from '../images/inter.png';
+
 function Produtos() {
   const produtos = [
     {
       nome: 'Palha Italiana Tradicional',
       descricao: 'A nossa palha italiana tradicional, com chocolate belga e leite condensado.',
       preco: 'R$ 15,00',
-      imagem: '/images/tradicional.avif', // Caminho correto para a pasta public/images
+      imagem: palhatradiconal, // Usando a imagem 'palhaninho.jpeg'
     },
     {
-      nome: 'Palha Italiana de Morango',
-      descricao: 'Uma deliciosa variação com morango fresco e chocolate branco.',
+      nome: 'Palha Italiana Ninho',
+      descricao: 'Uma deliciosa variação com leite ninho e chocolate brancox  .',
       preco: 'R$ 18,00',
-      imagem: '/images/tradicional.avif', // Usando a mesma imagem como exemplo
+      imagem: palhaninho, // Usando a imagem 'palhaninho.jpeg'
     },
     {
       nome: 'Palha Italiana de Chocolate',
       descricao: 'Feita com chocolate ao leite, para os amantes de chocolate.',
-      preco: 'R$ 17,00',
-      imagem: '/images/tradicional.avif',
+      preco: 'Em breve',
+      imagem: inter, // Usando a imagem 'palhaninho.jpeg'
+      emBreve: true, // Produto "Em breve"
     },
     {
       nome: 'Palha Italiana Gourmet',
       descricao: 'Uma versão gourmet com pistache e ganache de chocolate belga.',
-      preco: 'R$ 22,00',
-      imagem: '/images/tradicional.avif',
+      preco: 'Em breve',
+      imagem: inter, // Usando a imagem 'palhaninho.jpeg'
+      emBreve: true, // Produto "Em breve"
     },
   ];
 
@@ -34,12 +41,14 @@ function Produtos() {
       <h1>Nosso Cardápio</h1>
       <div className="produtos-list">
         {produtos.map((produto, index) => (
-          <div key={index} className="produto-card">
-            <img src={produto.imagem} alt={produto.nome} />
+          <div key={index} className={`produto-card ${produto.emBreve ? 'em-breve' : ''}`}>
+            <img src={produto.imagem} alt={produto.nome} className="produto-imagem" />
             <h3>{produto.nome}</h3>
             <p>{produto.descricao}</p>
             <span>{produto.preco}</span>
-            <button className="btn-comprar">Comprar</button>
+            <button className="btn-comprar">
+              {produto.emBreve ? 'Lançamento' : 'Comprar'}
+            </button>
           </div>
         ))}
       </div>
