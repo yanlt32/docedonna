811434c58a442de1d1fe39710b17eecd3e2329df
yan <ladeiatortelli8@outlook.com>
2025-01-28 16:30:07 -0300

    Mensagem explicativa do que foi feito

---
+++ src/components/Produtos.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import '../styles/Produtos.css'; // Importando o CSS para a estilização dos cards
+
+// Importando a mesma imagem para todos os produtos
+import palhaninho from '../images/palhaninho.jpeg';
+import palhatradiconal from '../images/palhatradicional.jpg';
+import inter from '../images/inter.png';
+
+function Produtos() {
+  const produtos = [
+    {
+      nome: 'Palha Italiana Tradicional',
+      descricao: 'A nossa palha italiana tradicional, com chocolate belga e leite condensado.',
+      preco: 'R$ 15,00',
+      imagem: palhatradiconal, // Usando a imagem 'palhaninho.jpeg'
+    },
+    {
+      nome: 'Palha Italiana Ninho',
+      descricao: 'Uma deliciosa variação com leite ninho e chocolate brancox  .',
+      preco: 'R$ 18,00',
+      imagem: palhaninho, // Usando a imagem 'palhaninho.jpeg'
+    },
+    {
+      nome: 'Palha Italiana de Nutella',
+      descricao: 'Feita com brigdeiro de nutella, para os amantes de doce.',
+      preco: 'Em breve',
+      imagem: inter, // Usando a imagem 'palhaninho.jpeg'
+      emBreve: true, // Produto "Em breve"
+    },
+    {
+      nome: 'Palha Italiana de oreo',
+      descricao: 'Uma versão da palha italiana com oreo e brigadeiro branco.',
+      preco: 'Em breve',
+      imagem: inter, // Usando a imagem 'palhaninho.jpeg'
+      emBreve: true, // Produto "Em breve"
+    },
+  ];
+
+  return (
+    <div className="produtos-container">
+      <h1>Nosso Cardápio</h1>
+      <div className="produtos-list">
+        {produtos.map((produto, index) => (
+          <div key={index} className={`produto-card ${produto.emBreve ? 'em-breve' : ''}`}>
+            <img src={produto.imagem} alt={produto.nome} className="produto-imagem" />
+            <h3>{produto.nome}</h3>
+            <p>{produto.descricao}</p>
+            <span>{produto.preco}</span>
+            <button className="btn-comprar">
+              {produto.emBreve ? 'Lançamento' : 'Comprar'}
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Produtos;
---
+++ src/components/QuemSomos.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/QuemSomos.css';
+
+function QuemSomos() {
+  return (
+    <div className="quem-somos-container">
+      <div className="quem-somos-header">
+        <h1>Quem Somos</h1>
+        <p>Somos uma empresa especializada na produção de Palha Italiana, oferecendo os melhores sabores e ingredientes para você.</p>
+      </div>
+      <div className="quem-somos-content">
+        <p>Com uma tradição que valoriza a qualidade e o sabor, buscamos trazer para você o melhor da Palha Italiana, feita com carinho e dedicação.</p>
+      </div>
+    </div>
+  );
+}
+
+export default QuemSomos;
